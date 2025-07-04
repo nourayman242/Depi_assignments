0404095cf73778bff3c046ac43f154c4d8e93827
nourayman242 <nourayman2422004@gmail.com>
2025-07-04 18:45:36 +0300

    assignment submission

--- src/Main.java
+++ src/Main.java
@@ -4,45 +4,62 @@ public class Main {
     public static void main(String[] args) {
         Scanner input=new Scanner(System.in);
 
-//        String line=input.nextLine();
-//        System.out.println("Uppercase: "+ line.toUpperCase());
-//        System.out.println("Length: "+ line.length());
-//        System.out.println("First Character: "+ line.charAt(0));
+        //Create a Java program that takes two numbers and performs:
+        //Addition
+        //Subtraction
+        //Multiplication
+        //Division
 
-//        System.out.println("Enter number 1 and number 2");
-//        double num1 = input.nextDouble();
-//        double num2 = input.nextDouble();
-//        System.out.println("Addition: "+ (num1+num2));
-//        System.out.println("Subtraction: "+ (num1-num2));
-//        System.out.println("Multiplication: "+ (num1*num2));
-//        System.out.println("Division: "+ (num1/num2));
+        System.out.println("Enter number 1 and number 2");
+        double num1 = input.nextDouble();
+        double num2 = input.nextDouble();
+        System.out.println("Addition: "+ (num1+num2));
+        System.out.println("Subtraction: "+ (num1-num2));
+        System.out.println("Multiplication: "+ (num1*num2));
+        System.out.println("Division: "+ (num1/num2));
 
-        System.out.println("Please enter your name: ");
-        String name= input.nextLine();
-        System.out.println("Please enter your age: ");
-        int age=input.nextInt();
-        input.nextLine();
-        System.out.println("Please enter your favourite programming language: ");
-        String lang= input.nextLine();
+// ***************************************************************************
+//        Read a sentence from the user and:
+//                Convert to uppercase
+//                Show how many characters it contains
+//                Show the first character
 
-        System.out.println("Hello! My name is "+name);
-        System.out.println("I am "+age+" years old, which is "+(age*12)+" months old");
-        System.out.println("May favourite programming language is "+ lang);
-        System.out.println("Next year i will be "+ (++age) +" years old");
+        String line=input.nextLine();
+        System.out.println("Uppercase: "+ line.toUpperCase());
+        System.out.println("Length: "+ line.length());
+        System.out.println("First Character: "+ line.charAt(0));
 
-//        int x=input.nextInt();
-//
-//        String y=input.next();
-//
-//        System.out.println(x+y);
+//**********************************************************************************
+        //Task : Check Voting Eligibility
 
-//        System.out.println(" Enter x");
-//        int x= input.nextInt();
-//        System.out.println(" Enter y");
-//        int y= input.nextInt();
-//        System.out.println("Are x and y even? "+ ((x%2==0)&&(y%2==0)));
-//        //System.out.println("Is x even? "+ (x%2==0));
-//        //System.out.println("Is y even? "+ (y%2==0));
+        System.out.println("Enter your age: ");
+        int age = input.nextInt();
+
+        System.out.println("Enter your nationality: ");
+        String nationality = input.next();
+
+        if(age>= 18 && nationality.equals("Egyptian")){
+            System.out.println("Eligible to vote");
+        }
+        else{
+            System.out.println("Not eligible");}
+
+//*******************************************************************************8
+            // Task:
+            //Write a Java program that:
+            //Asks the user to enter a number.
+            //If the number is even, print: "Even number"
+            //Else, print: "Odd number"
+
+        System.out.println("Please Enter a number: ");
+        int x= input.nextInt();
+        if(x%2==0){
+            System.out.println("Even number");
+        }
+        else{
+            System.out.println("Odd number");
+        }
+
 
         }
     }
